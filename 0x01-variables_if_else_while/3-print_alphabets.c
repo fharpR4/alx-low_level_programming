5b344bd8000e36abfc93635014e8e805a26559d7
fharpR4 <favourluckyte314@gmail.com>
2022-09-10 16:31:35 -0700

    nawa o

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - prints the alphabetic in lower and upper case
+* Return: Always (Success)
+*
+*/
+
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+}
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
