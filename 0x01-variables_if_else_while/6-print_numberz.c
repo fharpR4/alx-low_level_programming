b5743361b1d8882b88af7579c9acee70d755ce3e
fharpR4 <favourluckyte314@gmail.com>
2022-09-11 13:26:48 -0700

    How come

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success/Correct)
+ */
+
+int main(void)
+{
+int i;
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
