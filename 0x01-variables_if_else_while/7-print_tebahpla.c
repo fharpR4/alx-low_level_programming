54f6367aee72ab808db012bfaa4e9cb756e088e1
fharpR4 <favourluckyte314@gmail.com>
2022-09-11 14:40:44 -0700

    i m Free at last

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success/Correct)
+*/
+
+int main(void)
+{
+char alphabet;
+for (alphabet = 122 ; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
