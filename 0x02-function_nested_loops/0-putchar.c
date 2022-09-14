81c1762005fab5a868e836eb82ce734bf050ab95
fharpR4 <favourluckyte314@gmail.com>
2022-09-13 18:22:52 -0700

    Nawaa oh

---
+++ 0x02-function_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main-main block
+ * Description:print -putchar in a new line
+ * Return:this program returns zero
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
