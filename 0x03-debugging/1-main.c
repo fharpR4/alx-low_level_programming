013ad8ac8706b6983604855ffbbd056dca822730
fharpR4 <favourluckyte314@gmail.com>
2022-09-15 21:24:45 -0700

    0-1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,10 +10,13 @@ int main(void)
 int i;
 printf("Infinite loop incoming :(\n");
 i = 0;
-/* while (i < 10)  */
-/*{*/
-/*putchar(i);*/
-/*}*/
+/*
+ * while (i < 10)
+ * {
+ * Infinite loop - No increase of variable
+ * putchar(i);
+ *  }
+ */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,8 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
-
-
 #include <stdio.h>
 
-
-
-void positive_or_negative(int i);
+void positive_or_negative(int n);
 
 int largest_number(int a, int b, int c);
 
